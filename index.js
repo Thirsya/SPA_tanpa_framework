fd334464f39342710aacafe92639f1d95e00c44b
Thirsya <thirsya.widya@gmail.com>
2023-02-13 09:37:22 +0700

    client side routing

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ linkHome.onclick = function(event) {
     event.preventDefault();
     root.textContent = "Welcome to Home";
     history.pushState(null, "", event.target.href);
-}
+};
 
 linkAbout.onclick = function(event) {
     event.preventDefault();
     root.textContent = "Welcome to About";
     history.pushState(null, "", event.target.href);
+};
+
+if (location.hash == "#about"){
+    root.textContent = "Welcome to About";
+} else if (location.hash == "#home"){
+    root.textContent = "Welcome to Home";
 }
